c4e040646d8ebcd2a162fd341a2de26a43979bdc
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-07-14 03:10:15 +0800

    add init

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import http from 'http';
+import { Server as SocketIO } from 'socket.io';
+import cors from 'cors';
+
+const PORT = 4000;
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIO(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('message', (message) => {
+    console.log('Message received:', message);
+    io.emit('message', message);
+  });
+
+  socket.on('typing', (username) => {
+    socket.broadcast.emit('typing', username);
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
